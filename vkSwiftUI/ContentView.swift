3a1c3661d94feb1b29ea872a308d717afa3a1cee
Ke4a <helpomos@gmail.com>
2022-07-11 19:51:16 +0300

    Initial Commit

---
+++ vkSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  vkSwiftUI
+//
+//  Created by Ke4a on 11.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ vkSwiftUI/vkSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  vkSwiftUIApp.swift
+//  vkSwiftUI
+//
+//  Created by Ke4a on 11.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct vkSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
